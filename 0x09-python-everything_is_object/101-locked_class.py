f465884d689098dfe32a999e50d3beb33fd5d20b
Tafadzwa-Charakupa <charakupatafah2@gmail.com>
2023-07-04 06:29:35 -0700

    Corrections

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -4,7 +4,8 @@
 
 
 class LockedClass:
-    """ Prevent the user from instantiating new LockedClass
+    """
+    Prevent the user from instantiating new LockedClass
     attributes for anything but attributes called 'first_name'.
     """
 

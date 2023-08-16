c887f7f04c8b3576710324fd3df55db0a202a922
Tafadzwa-Charakupa <charakupatafah2@gmail.com>
2023-08-16 01:55:28 -0700

    SQL

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,3 +1,2 @@
--- a script that displays the number of records
--- with id = 89 in the table first_table of the database hbtn_0c_0 in your MySQL server.
+-- displays number of records with id = 89 in the table first_table of db hbtn_0c_0
 SELECT count(*) FROM first_table where id = 89;
--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,4 +1,3 @@
--- a script that creates a table second_table in the database hbtn_0c_0 in your MySQL server 
--- and add multiples rows
+-- creates a table second_table in the db hbtn_0c_0  &  add multiples rows
 CREATE table IF NOT EXISTS second_table('id' INT, 'name' VARCHAR(256), 'score' INT);
 INSERT INTO second_table(id, name, score) VALUES(1, 'John', 10), (2, 'Alex', 3), (3, 'Bob', 14), (4, 'George', 8);
